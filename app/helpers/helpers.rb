d000694838fb90be325753023f574de65485e033
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-16 08:25:02 +0000

    Automatically backed up by Learn

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,7 +1,7 @@
 require 'sinatra/base'
 class Helpers
   def self.current_user
-    session[:user_id] = user.id
+    @user = User.find_by(session[:user_id] = user.id)
   end
   
   def is_logged_in?
