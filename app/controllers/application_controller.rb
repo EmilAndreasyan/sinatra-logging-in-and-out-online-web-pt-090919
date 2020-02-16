8886b2c7642315e0c0cbe0a6b8232264a2a9e804
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-16 08:19:51 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,8 @@ class ApplicationController < Sinatra::Base
   if @user != nil && @user.password == params[:password]
     session[:user_id] = user.id
     redirect_to "/account"
-  else
-    redirect_to "/error"
   end
+    erb :error
   end
 
   get '/account' do
